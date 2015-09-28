33d17cb071a7a4c0436cf10257a9a6ae9c58cdd9
btbailey <brandon.bailey@slalom.com>
2015-09-27 23:09:10 -0500

    Implemnted scss build with grunt

--- CS-boilerplate/Gruntfile.js
+++ CS-boilerplate/Gruntfile.js
@@ -2,13 +2,20 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     
+    watch: {
+      css: {
+        files: './public/stylesheets/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    
     sass: {
       dist: {
         options: {
           style: 'expanded'
         },
         files: {
-          './public/stylesheets/main-test.css': './public/stylesheets/main-test.scss'
+          './public/stylesheets/main.css': './public/stylesheets/main.scss'
         }
       }
     }
@@ -16,6 +23,8 @@ module.exports = function(grunt) {
 
   // load all grunt tasks
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   
-  grunt.registerTask('default', ['sass']);
+  
+  grunt.registerTask('default', ['sass', 'watch']);
 };
