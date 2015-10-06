57a03e91243bacd7e34e001dbd405e73c9c775df
btbailey <brandon.bailey@slalom.com>
2015-10-06 07:28:31 -0500

    test.sh can log in to heroku

--- CS-boilerplate/test.sh
+++ CS-boilerplate/test.sh
@@ -5,11 +5,15 @@ emailregex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([
 echo Volume path to test:
 if df | awk '{print $9}' | grep -Ex $cspubkeys > /dev/null; then
   echo "Drive mouted"
-  eval ls /Volumes/TEST_POC_CS
+  eval ls $cspubkeys
   echo -n "Enter email address for github account "
   read emailaddress
   if [[ $emailaddress =~ $emailregex ]] ; then
     echo "email is $emailaddress"
+    studentpath=${emailaddress%@*};
+    echo "$studentpath"
+    eval ls $cspubkeys"/"$studentpath
+    eval heroku login
   else
     echo "No valid email entered, try again"
   fi
