5340169eade459d817507b8f61e8c6d15e553cf2
btbailey <brandon.bailey@slalom.com>
2015-10-06 07:02:49 -0500

    testing script able to check for loaded usb and valid email address

---
+++ CS-boilerplate/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cspubkeys="/Volumes/TEST_POC_CS";
+emailregex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$";
+
+echo Volume path to test:
+if df | awk '{print $9}' | grep -Ex $cspubkeys > /dev/null; then
+  echo "Drive mouted"
+  eval ls /Volumes/TEST_POC_CS
+  echo -n "Enter email address for github account "
+  read emailaddress
+  if [[ $emailaddress =~ $emailregex ]] ; then
+    echo "email is $emailaddress"
+  else
+    echo "No valid email entered, try again"
+  fi
+else
+  echo "Drive not mounted do nothing"
+fi
